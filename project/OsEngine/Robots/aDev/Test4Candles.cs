b0c90b1097499db97c9e927f7409d5a3faaec382
Alexey Averyanov <averyanovalexey@gmail.com>
2020-02-16 01:17:23 +0600

    (dev) Продолжил. интересная идея. 6 свечей, 1 прокол. Профит-Фактор =3,6!

--- project/OsEngine/Robots/aDev/Test4Candles.cs
+++ project/OsEngine/Robots/aDev/Test4Candles.cs
@@ -7,7 +7,6 @@ using OsEngine.Entity;
 using OsEngine.Market;
 using OsEngine.OsTrader.Panels;
 using OsEngine.OsTrader.Panels.Tab;
-using System.Windows.Forms.DataVisualization.Charting;
 
 namespace OsEngine.Robots.aDev
 {
@@ -15,6 +14,8 @@ namespace OsEngine.Robots.aDev
     {
 
         private BotTabSimple tab0;
+
+        private DateTime timeStopOrder;
 
 
         public Test4Candles(string name, StartProgram startProgram) : base(name, startProgram)
@@ -67,22 +68,26 @@ namespace OsEngine.Robots.aDev
             tab0.SetChartElement(lineOnChart);
         }
 
-        private decimal minVal(decimal val1, decimal val2, decimal val3, decimal val4)
+        private decimal minVal(decimal val1, decimal val2, decimal val3, decimal val4, decimal val5, decimal val6)
         {
             var min = val1;
             if (val2 < min) min = val2;
             if (val3 < min) min = val3;
             if (val4 < min) min = val4;
+            if (val5 < min) min = val5;
+            if (val6 < min) min = val6;
 
             return min;
         }
 
-        private decimal maxVal(decimal val1, decimal val2, decimal val3, decimal val4)
+        private decimal maxVal(decimal val1, decimal val2, decimal val3, decimal val4, decimal val5, decimal val6)
         {
             var max = val1;
             if (val2 > max) max = val2;
             if (val3 > max) max = val3;
             if (val4 > max) max = val4;
+            if (val5 > max) max = val5;
+            if (val6 > max) max = val6;
 
             return max;
         }
@@ -94,20 +99,30 @@ namespace OsEngine.Robots.aDev
             var slack = 3;
 
 
-            List<Position> positions = TabsSimple[0].PositionsOpenAll;
+            List<Position> positions = tab0.PositionsOpenAll;
             if (positions != null && positions.Count != 0)
             {
+                
+                //if (positions[0].State == PositionStateType.Opening && tab0.TimeServerCurrent >= timeStopOrder)
+                //{
+                //    tab0.CloseAllOrderToPosition(positions[0]);
+                //}
+                
+ 
+                
                 return;
             }
 
 
-            if (candles.Count < 5) return;
+            if (candles.Count < 7) return;
 
 
-            var candle1 = candles[candles.Count - 4];
-            var candle2 = candles[candles.Count - 3];
-            var candle3 = candles[candles.Count - 2];
-            var candle4 = candles[candles.Count - 1];
+            var candle1 = candles[candles.Count - 6];
+            var candle2 = candles[candles.Count - 5];
+            var candle3 = candles[candles.Count - 4];
+            var candle4 = candles[candles.Count - 3];
+            var candle5 = candles[candles.Count - 2];
+            var candle6 = candles[candles.Count - 1];
 
 
             //проверяем на Low
@@ -115,29 +130,71 @@ namespace OsEngine.Robots.aDev
             var low2 = candle2.Low;
             var low3 = candle3.Low;
             var low4 = candle4.Low;
+            var low5 = candle5.Low;
+            var low6 = candle6.Low;
 
-            var min = minVal(low1, low2, low3, low4);
+            var body1 = Math.Min(candle1.Close, candle1.Open);
+            var body2 = Math.Min(candle2.Close, candle2.Open);
+            var body3 = Math.Min(candle3.Close, candle3.Open);
+            var body4 = Math.Min(candle4.Close, candle4.Open);
+            var body5 = Math.Min(candle5.Close, candle5.Open);
+            var body6 = Math.Min(candle6.Close, candle6.Open);
 
-            var delta1 = Math.Abs(low1 - min);
-            var delta2 = Math.Abs(low2 - min);
-            var delta3 = Math.Abs(low3 - min);
-            var delta4 = Math.Abs(low4 - min);
+            var checkPrice = maxVal(low1, low2, low3, low4, low5, low6);
 
-            if ((delta1 <= slack) && (delta2 <= slack) && (delta3 <= slack) && (delta4 <= slack))
+            var delta1 = Math.Abs(low1 - checkPrice);
+            var delta2 = Math.Abs(low2 - checkPrice);
+            var delta3 = Math.Abs(low3 - checkPrice);
+            var delta4 = Math.Abs(low4 - checkPrice);
+            var delta5 = Math.Abs(low3 - checkPrice);
+            var delta6 = Math.Abs(low4 - checkPrice);
+
+            var touch = 0;
+            var prokol = 0;
+            var error = 0;
+
+
+            if (delta1 <= slack && body1 > checkPrice) touch++;
+            else if (body1 > checkPrice && low1 < checkPrice) prokol++;
+            else error++;
+
+            if (delta2 <= slack && body2 > checkPrice) touch++;
+            else if (body2 > checkPrice && low2 < checkPrice) prokol++;
+            else error++;
+
+            if (delta3 <= slack && body3 > checkPrice) touch++;
+            else if (body3 > checkPrice && low3 < checkPrice) prokol++;
+            else error++;
+
+            if (delta4 <= slack && body4 > checkPrice) touch++;
+            else if (body4 > checkPrice && low4 < checkPrice) prokol++;
+            else error++;
+
+            if (delta5 <= slack && body5 > checkPrice) touch++;
+            else if (body5 > checkPrice && low5 < checkPrice) prokol++;
+            else error++;
+
+            if (delta6 <= slack && body6 > checkPrice) touch++;
+            else if (body6 > checkPrice && low6 < checkPrice) prokol++;
+            else error++;
+
+
+            if (touch >= 4 && prokol <= 2 && error == 0)
             {
-                DrawLine(min, $"line-{Convert.ToString(candle1.TimeStart)}", candle1.TimeStart, candle4.TimeStart, Color.Blue);
-                tab0.BuyAtMarket(1);
+                DrawLine(checkPrice, $"line-{Convert.ToString(candle1.TimeStart)}", candle1.TimeStart, candle6.TimeStart, Color.Blue);
+                tab0.BuyAtLimit(1, checkPrice + slack);
                 return;
             }
+            
 
-
+            /*
             //проверяем на High
             var high1 = candle1.High;
             var high2 = candle2.High;
             var high3 = candle3.High;
             var high4 = candle4.High;
 
-            var max = maxVal(high1, high2, high3, high4);
+            var max = minVal(high1, high2, high3, high4);
 
             delta1 = Math.Abs(high1 - max);
             delta2 = Math.Abs(high2 - max);
@@ -147,11 +204,12 @@ namespace OsEngine.Robots.aDev
             if ((delta1 <= slack) && (delta2 <= slack) && (delta3 <= slack) && (delta4 <= slack))
             {
                 DrawLine(max, $"line-{Convert.ToString(candle1.TimeStart)}", candle1.TimeStart, candle4.TimeStart, Color.Green);
-                tab0.SellAtMarket(1);
+                tab0.SellAtLimit(1, max - slack);
+                timeStopOrder = tab0.TimeServerCurrent.AddSeconds(tab0.TimeFrame.TotalSeconds * 2);
             }
 
 
-
+            */
 
         }
 
